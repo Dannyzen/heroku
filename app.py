885690d484dad89b25715162afa66a2a07980186
Danny <danny.rosen@gmail.com>
2012-08-18 22:48:36 -0400

    changing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'This was way too easy'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
